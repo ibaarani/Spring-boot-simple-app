f2359ed195780e8e29ad15d77022cb94ef306d49
ibaarani <dharabarani@gmail.com>
2021-11-08 13:15:09 +0530

    flyway migration done

---
+++ src/main/java/com/induction/secduletask/secdule.java
@@ -0,0 +1,20 @@
+package com.induction.secduletask;
+
+
+import com.induction.Baranidharan.StudentRepository;
+import com.induction.Baranidharan.testdb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Calendar;
+
+
+public class secdule {
+
+    @Autowired
+    private StudentRepository ss;
+
+
+}
+
+
