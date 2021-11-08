d37587305225737b46dff28f143572e54c41b68c
ibaarani <dharabarani@gmail.com>
2021-11-08 15:41:45 +0530

    corn schedular done

--- src/main/java/com/induction/Baranidharan/BaranidharanApplication.java
+++ src/main/java/com/induction/Baranidharan/BaranidharanApplication.java
@@ -1,15 +1,39 @@
 package com.induction.Baranidharan;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.persistence.ManyToOne;
+import java.util.Calendar;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class BaranidharanApplication {
+
+	@Autowired
+	StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaranidharanApplication.class, args);
 
 
 	}
+	@Scheduled(fixedDelay = 10000)
+	public void run() {
+
+		testdb dr = new testdb();
+		int a = 100;
+		dr.setId(++a);
+		dr.setName("baani");
+		dr.setEmail("hey google");
+
+		studentRepository.save(dr);
+
+	}
 
 }
+
--- src/main/java/com/induction/secduletask/secdule.java
+++ src/main/java/com/induction/secduletask/secdule.java
@@ -4,17 +4,31 @@ package com.induction.secduletask;
 import com.induction.Baranidharan.StudentRepository;
 import com.induction.Baranidharan.testdb;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
-
+@EnableScheduling
 public class secdule {
 
     @Autowired
     private StudentRepository ss;
+    private  testdb s;
 
 
+    @Component
+    public class Scheduler {
+        @Scheduled(fixedDelay = 10000)
+        public void run() {
+            System.out.print("Current time is :: " + Calendar.getInstance().getTime());
+        }
+    }
 }
+
 
 
